3e6253b61c49fdce1122fbc005d00976e4cab5b4
alford <alford.099@gmail.com>
2018-10-18 12:32:03 +0800

    add tingyun

--- app/src/main/java/com/alford/apmevaluation/MainActivity.java
+++ app/src/main/java/com/alford/apmevaluation/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.networkbench.agent.impl.NBSAppAgent;
+
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -23,6 +25,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        NBSAppAgent.setLicenseKey("bbf6d85620c44a779feceed374da2855")
+                .withLocationServiceEnabled(true)
+                .start(this.getApplicationContext());//Appkey 请从官网获取
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.btn_http_get).setOnClickListener(this);
