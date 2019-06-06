43b8b42f9d8adf8fa9c225c555c3cd58beeb49c4
877887 <877887@nd.com>
2019-06-06 16:59:53 +0800

    升级库

--- app/src/main/java/com/alford/apmevaluation/MainActivity.java
+++ app/src/main/java/com/alford/apmevaluation/MainActivity.java
@@ -1,12 +1,9 @@
 package com.alford.apmevaluation;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import com.newrelic.agent.android.NewRelic;
 
@@ -18,7 +15,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends Activity implements View.OnClickListener {
 
 
     private static final String TAG = "MainActivity";
