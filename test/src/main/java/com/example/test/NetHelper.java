2935006e29e46cdb96982747b8a17811b086d69a
877887 <877887@nd.com>
2019-08-15 15:52:34 +0800

    firebase集成

---
+++ test/src/main/java/com/example/test/NetHelper.java
@@ -0,0 +1,36 @@
+package com.example.test;
+
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class NetHelper {
+    private final String TAG = "NetHelper";
+
+    public void request() {
+        OkHttpClient client = new OkHttpClient();
+        //构造Request对象
+        //采用建造者模式，链式调用指明进行Get请求,传入Get的请求地址
+        Request request = new Request.Builder().get().url("https://www.baidu.com").build();
+        Call call = client.newCall(request);
+        //异步调用并设置回调函数
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, e.toString());
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                final String responseStr = response.body().string();
+                Log.e(TAG, responseStr);
+            }
+        });
+    }
+}
